9be2f183a8ff714047829fec8cf94b9dc39b682f
minipekka25 <shankartreeks@gmail.com>
2020-07-17 22:21:21 +0530

    test
    jff

---
+++ precache-manifest.93a68cae1e137421837d4340781d98a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e5816d91301fe0561cf1ea5e24d14a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11ea988764b5b36702ad",
+    "url": "./static/css/main.8d78ae90.chunk.css"
+  },
+  {
+    "revision": "c7a9a93a88b126b0698f",
+    "url": "./static/js/2.10b15772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.10b15772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ea988764b5b36702ad",
+    "url": "./static/js/main.ba6264be.chunk.js"
+  },
+  {
+    "revision": "eb880aac70f6d7e67a0a",
+    "url": "./static/js/runtime-main.c66efacf.js"
+  }
+]);
--- wp-content/themes/lapa/searchform.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-?><form method="get" class="search-form" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
-	<input autocomplete="off" type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search here&hellip;', 'front-view', 'lapa' ); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'front-view', 'lapa' ); ?>" />
-	<button class="search-button" type="submit"><i class="dl-icon-search10"></i></button>
-</form>
-<!-- .search-form -->
